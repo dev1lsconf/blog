5ad90d27346d6824ba67adf704b573331113a4d3
dev1ls <dev1lsconf@gmail.com>
2023-08-20 00:37:50 -0400

    cambio en el slogan

--- main.tsx
+++ main.tsx
@@ -4,10 +4,10 @@ import blog, { ga, redirects, h } from "blog";
 
 blog({
   title: "Dev1ls>Blog",
-  description: "OpenBSD & NixOS User..  still learning",
+  description: "BSD & Linux User..  still learning",
   // header: <header>Dev1ls' Blog</header>,
   // section: <section>My Logs:</section>,
- // `` footer: <footer>contact: dev1ls@sdf.org</footer>,
+  // `` footer: <footer>contact: dev1ls@sdf.org</footer>,
   avatar: "beastie.png",
   avatarClass: "rounded-full",
   author: "Dev1ls",
@@ -16,19 +16,19 @@ blog({
     { title: "Email", url: "mailto:dev1ls@texto-plano.xyz" },
     { title: "GitHub", url: "https://github.com/dev1lsconf" },
     { title: "Twitter", url: "https://twitter.com/dev1lsconf" },
-    { title: "Mastodon", url: "https://mastodon.sdf.org/@dev1ls"},
+    { title: "Mastodon", url: "https://mastodon.sdf.org/@dev1ls" },
 
   ],
   // middlewares: [
 
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
 
   // ]
 
